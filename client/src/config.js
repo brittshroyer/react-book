1c454ccc8d40235501b020ac43c67b34a06360a3
brittshroyer <britt.shroyer@gmail.com>
2019-02-14 00:59:52 -0600

    remove proxy

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 // export const API_URL = 'https://react-myspace.herokuapp.com';
-export const API_URL = 'http://localhost:8000/';
+export const API_URL = 'http://localhost:8000';
 export const AUTH_CLIENT_ID = 'mLTG75qv2vnwgst1rFh6i6R6pY8Lq15J';
